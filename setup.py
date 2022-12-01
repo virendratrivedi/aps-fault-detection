f6d594e74a3c972fbf2d734c2cf5ed8470aafdd4
virendratrivedi <trivedi.virendra2685mmi@gmail.com>
2022-12-01 23:39:01 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 from setuptools import find_packages,setup
+
 from typing import List
 
-REQUIREMENT_FILE_NAME='requirements.txt'
-HYPEN_E_DOT='-e .'
+REQUIREMENT_FILE_NAME="requirements.txt"
+HYPHEN_E_DOT = "-e ."
 
-def get_requirements()->List(str):
-    with open(REQUIREMENT_FILE_NAME) as requirements_file:
-        requirnment_list=requirements_file.readlines()
-        [requirement_name.replace('\n', '') for requirement_name in requirnment_list]
-        if HYPEN_E_DOT in requirnment_list:
-            requirnment_list.remove(HYPEN_E_DOT)
-        return requirnment_list    
+def get_requirements()->List[str]:
     
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+    requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+    
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
+
 
 
 setup(
     name="sensor",
-    version="0.0.1",
-    author="Arpit Trivedi",
-    author_email="trivedi.virendra2685mmi@gmail.com",
+    version="0.0.2",
+    author="ineuron",
+    author_email="avnish@ineuron.ai",
     packages = find_packages(),
     install_requires=get_requirements(),
 )
